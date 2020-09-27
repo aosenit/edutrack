161c1e7c089ead66174917e6aa878f040136bc24
unknown <pelumioni25@gmail.com>
2020-09-27 15:03:12 -0700

    m

---
+++ src/app/teacher/teacher.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TeacherRoutingModule } from './teacher-routing.module';
+import { TeacherComponent } from './teacher.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AssignmentComponent } from './assignment/assignment.component';
+import { AssignmentsComponent } from './assignments/assignments.component';
+import { CreateAssignmentComponent } from './create-assignment/create-assignment.component';
+import { FileManagerComponent } from './file-manager/file-manager.component';
+import { GradingComponent } from './grading/grading.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ViewFileComponent } from './view-file/view-file.component';
+import { VirtualClassComponent } from './virtual-class/virtual-class.component';
+import { VirtualSessionComponent } from './virtual-session/virtual-session.component';
+
+
+@NgModule({
+  declarations: [TeacherComponent, DashboardComponent, AssignmentComponent, AssignmentsComponent, CreateAssignmentComponent, FileManagerComponent, GradingComponent, ScheduleComponent, ViewFileComponent, VirtualClassComponent, VirtualSessionComponent],
+  imports: [
+    CommonModule,
+    TeacherRoutingModule
+  ]
+})
+export class TeacherModule { }
---
+++ src/app/teacher/virtual-class/virtual-class.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-virtual-class',
+  templateUrl: './virtual-class.component.html',
+  styleUrls: ['./virtual-class.component.css']
+})
+export class VirtualClassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
