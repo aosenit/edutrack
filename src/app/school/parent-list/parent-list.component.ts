1509d67b794a79c8e941a9a11f10f8601153b978
unknown <pelumioni25@gmail.com>
2020-10-02 13:01:54 -0700

    m

--- src/app/school/parent-list/parent-list.component.ts
+++ src/app/school/parent-list/parent-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent-list.component.css']
 })
 export class ParentListComponent implements OnInit {
-  records = false;
+  record = false;
   constructor() { }
 
   ngOnInit() {
--- src/app/school/student-list/student-list.component.ts
+++ src/app/school/student-list/student-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentListComponent implements OnInit {
 
-  records = false;
+  record = false;
   constructor() { }
 
   ngOnInit() {
