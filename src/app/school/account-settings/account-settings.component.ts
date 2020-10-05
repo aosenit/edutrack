52d3255a5f0acb52fa13da6827aea8bc8ee821bd
Alaketu Olamilekan <alaketuolamilekan02@gmail.com>
2020-10-05 12:44:01 +0100

    fix modals on settings and its submenu

--- src/app/school/account-settings/account-settings.component.ts
+++ src/app/school/account-settings/account-settings.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-settings.component.css']
 })
 export class AccountSettingsComponent implements OnInit {
-
+  users = true;
+  roles = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  showRoles() {
+    // tslint:disable-next-line:no-unused-expression
+    this.users = false;
+    this.roles = true;
+  }
+
+  showUsers() {
+    this.users = true;
+    this.roles = false;
+  }
 }
--- src/app/school/calendar/calendar.component.ts
+++ src/app/school/calendar/calendar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalendarComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
