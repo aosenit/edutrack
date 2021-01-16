1a926af042fb2871e36768b7e7e8643376353791
Alaketu Olamilekan <alaketuolamilekan02@gmail.com>
2021-01-16 16:53:01 +0100

    setup email verification page

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuardGuard } from 'src/services/guards/auth-guard.guard';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { EmailVerifiedComponent } from './email-verified/email-verified.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { SchoolLoginComponent } from './school-login/school-login.component';
 import { StudentLoginComponent } from './student-login/student-login.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   {path: 'student/login', component: StudentLoginComponent},
   {path: 'school/login', component: SchoolLoginComponent},
   {path: 'forgotpassword', component: ForgotPasswordComponent},
+  {path: 'email-verified', component: EmailVerifiedComponent},
 ];
 
 @NgModule({
