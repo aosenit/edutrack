831c70fedfc204cabc48dc626d5b1e8ed9a01400
Alaketu Olamilekan Soliu <alaketuolamilekan02@gmail.com>
2022-08-26 11:18:59 +0100

    Add report integration

--- src/app/school/reporting/reporting.component.ts
+++ src/app/school/reporting/reporting.component.ts
@@ -12,41 +12,41 @@ export class ReportingComponent implements OnInit {
   reportingOptions = [
     {id: 1, title: 'User profile', slug: 'userReport', data : [
       {id: 1, title: 'Export Student Profile'},
-      {id: 2, title: 'Export Teacher Profile'},
-      {id: 3, title: 'Export Non Teaching Staff profile'},
+      {id: 2, title: 'Teacher Profile'},
+      {id: 3, title: 'Non Teaching Staff profile'},
       {id: 4, title: 'Parent Profile'},
 
     ]},
-    {id: 2, title: 'Student Report', slug: 'studentReport', data: [
-      {id: 1, title: 'Daily Student Attendance'},
+    {id: 2, title: 'Attendance Report', slug: 'attendanceReport', data: [
+      {id: 1, title: 'Student Attendance'},
       {id: 2, title: 'Weekly Student Attendance'},
       {id: 3, title: 'Term Student Attendance'},
       {id: 4, title: 'Session Student Attendance'},
 
     ]},
-    {id: 3, title: 'Library Report', slug: 'libraryReport', data: [
-      {id: 1, title: 'Borrowed Book List'},
-      {id: 2, title: 'Borrowed Overdue List'},
+    // {id: 3, title: 'Library Report', slug: 'libraryReport', data: [
+    //   {id: 1, title: 'Borrowed Book List'},
+    //   {id: 2, title: 'Borrowed Overdue List'},
 
-    ]},
-    {id: 4, title: 'Finance Report', slug: 'financeReport', data: [
-      {id: 1, title: 'Journal Listing / Transactions'},
-      {id: 1, title: 'Monthly Depreciating Report'},
-      {id: 1, title: 'Annual Depreciating Report'},
-      {id: 1, title: 'Trial Balance'},
-      {id: 1, title: 'Profit & Loss Statement  '},
-      {id: 1, title: 'Balance Sheet  '},
-      {id: 1, title: 'Cash Book  '},
-      {id: 1, title: 'Vendor Line item Report  '},
-      {id: 1, title: 'Receivable Line item Report  '},
-      {id: 1, title: 'Asset Report  '},
-      {id: 1, title: 'Witholding Tax Report  '},
-      {id: 1, title: 'Inventory Report  '},
-      {id: 1, title: 'Parent Line item Report  '},
-      {id: 1, title: 'Spend Report  '},
-      {id: 1, title: 'Budget / Project Stewardship Report  '},
-      {id: 1, title: 'Project Line item Report  '}
-    ]},
+    // ]},
+    // {id: 4, title: 'Finance Report', slug: 'financeReport', data: [
+    //   {id: 1, title: 'Journal Listing / Transactions'},
+    //   {id: 1, title: 'Monthly Depreciating Report'},
+    //   {id: 1, title: 'Annual Depreciating Report'},
+    //   {id: 1, title: 'Trial Balance'},
+    //   {id: 1, title: 'Profit & Loss Statement  '},
+    //   {id: 1, title: 'Balance Sheet  '},
+    //   {id: 1, title: 'Cash Book  '},
+    //   {id: 1, title: 'Vendor Line item Report  '},
+    //   {id: 1, title: 'Receivable Line item Report  '},
+    //   {id: 1, title: 'Asset Report  '},
+    //   {id: 1, title: 'Witholding Tax Report  '},
+    //   {id: 1, title: 'Inventory Report  '},
+    //   {id: 1, title: 'Parent Line item Report  '},
+    //   {id: 1, title: 'Spend Report  '},
+    //   {id: 1, title: 'Budget / Project Stewardship Report  '},
+    //   {id: 1, title: 'Project Line item Report  '}
+    // ]},
   ];
 
 
@@ -94,6 +94,9 @@ export class ReportingComponent implements OnInit {
   selectReportType(event) {
     this.showNext = true;
     this.selectedSubReport = event;
+    // if (event === ) {
+
+    // }
     this.fetchAttendanceRecord(this.adminDetails.TenantId);
   }
 
