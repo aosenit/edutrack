2e65a0f4538dc0ef97a02cb342ca7f67c7ef06ef
Alaketu Olamilekan Soliu <alaketuolamilekan02@gmail.com>
2022-09-01 15:44:13 +0100

    task(report): Update report on teachers end

--- src/app/shared/homenavbar/homenavbar.component.ts
+++ src/app/shared/homenavbar/homenavbar.component.ts
@@ -31,7 +31,7 @@ export class HomenavbarComponent implements OnInit {
     const domain = window.location.hostname;
     // // (domain)
     if (domain.indexOf('.') < 0 ||
-      domain.split('.')[0] === 'example' || domain.split('.')[0] === 'lvh' || domain.split('.')[0] === 'www') {
+      domain.split('.')[0] === 'example' || domain.split('.')[0] === 'lvh' || domain.split('.')[0] === 'www' || domain.split('.')[0] === 'myschooltrack') {
       this.subdomain = '';
       this.displayAdminLogin = true;
       this.displayOtherLogin = false;
