2dc8f5298b4efe9290e1dd75e6b89bb43843e5a8
Alaketu Olamilekan Soliu <alaketuolamilekan02@gmail.com>
2022-12-09 15:28:29 +0100

    task(env): Update production baseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
   productLogoLarge: 'assets/images/svg/logo.svg',
-   serverUrl: 'http://13.88.13.159:58100/',
+  serverUrl: 'http://65.21.111.173:58100/',
+  //  serverUrl: 'http://13.88.13.159:58100/',
   //  serverUrl: 'https://backend.myschooltrack.com/',
    demourl: 'http://533db88f5fb7.ngrok.io/'
 
