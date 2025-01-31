6a121b566d9d7bf7e68076d67f762aadc67b8ee7
Emmanuel Asimobi <asimobiemmanuel@gmail.com>
2025-01-31 10:05:29 +0100

    Display all Logins on Test

--- src/app/shared/homenavbar/homenavbar.component.ts
+++ src/app/shared/homenavbar/homenavbar.component.ts
@@ -30,8 +30,14 @@ export class HomenavbarComponent implements OnInit {
   getSubdomain() {
     const domain = window.location.hostname;
     // // (domain)
-    if (domain.indexOf('.') < 0 ||
-      domain.split('.')[0] === 'example' || domain.split('.')[0] === 'lvh' || domain.split('.')[0] === 'www' || domain.split('.')[0] === 'myschooltrack') {
+    if (domain.indexOf('.') < 0 || domain.split('.')[0] === 'edutrack-black') {
+      this.subdomain = domain.split('.')[0];
+      localStorage.setItem('sub-domain', this.subdomain);
+      this.displayAdminLogin = true;
+      this.displayOtherLogin = true;
+    }
+    else if (domain.indexOf('.') < 0 ||
+      domain.split('.')[0] === 'example' || domain.split('.')[0] === 'lvh' || domain.split('.')[0] === 'www' || domain.split('.')[0] === 'myEduTrack') {
       this.subdomain = '';
       this.displayAdminLogin = true;
       this.displayOtherLogin = false;
